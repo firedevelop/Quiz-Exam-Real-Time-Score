c884549d4a0f13984f9d188f39d7b707f5e5878d
fireDevelop <firedevelop.com@gmail.com>
2024-12-31 20:47:12 +0100

    .
    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { questions } from './questions.js';
+const subjectSelect = document.getElementById('subject-select');
 
 const quizContainer = document.getElementById('quiz-container');
 const submitButton = document.getElementById('submit-quiz');
@@ -19,9 +20,11 @@ const checkCorrectsSimplyButton = document.getElementById('check-corrects-simply
 
 let questionsAnswered = 0;
 let correctAnswers = 0;
-const userAnswers = new Array(questions.length).fill(null);
+let userAnswers = [];
 let showExplanations = false;
 let alwaysShowExplanations = false;
+
+let currentQuestions = questions; // set initial questions
 
 showExplanationsCheckbox.addEventListener('change', (event) => {
     showExplanations = event.target.checked;
@@ -33,52 +36,91 @@ alwaysShowExplanationsCheckbox.addEventListener('change', (event) => {
     updateExplanationsVisibility();
 });
 
+
+subjectSelect.addEventListener('change', async (event) => {
+    const selectedSubject = event.target.value;
+    console.log("selected subject: ", selectedSubject)
+     await loadQuestions(selectedSubject);
+    resetQuiz();
+    buildQuiz();
+});
 filterCorrectButton.addEventListener('click', () => filterQuestions('correct'));
 filterIncorrectButton.addEventListener('click', () => filterQuestions('incorrect'));
 filterEmptyButton.addEventListener('click', () => filterQuestions('empty'));
 filterAllButton.addEventListener('click', () => filterQuestions('all'));
 checkCorrectsButton.addEventListener('click', () => checkAllCorrectAnswers(false));
-checkCorrectsSimplyButton.addEventListener('click', () => checkAllCorrectAnswers(true));
+ checkCorrectsSimplyButton.addEventListener('click', () => checkAllCorrectAnswers(true));
+
+async function loadQuestions(subject) {
+    try {
+         const currentDir = import.meta.url.substring(0, import.meta.url.lastIndexOf('/'))
+          const module = await import(`${currentDir}/${subject}.js`);
+        currentQuestions = module.questions;
+        userAnswers = new Array(currentQuestions.length).fill(null);
+       updateScoreDisplay()
+    } catch (error) {
+         console.error('Failed to load questions:', error);
+          // Use the alert function to check the full error
+          alert(`Error loading questions for subject ${subject}: ${error.message}`);
+        currentQuestions = [];
+    }
+}
+
 
 function buildQuiz() {
-    questions.forEach((question, index) => {
-        const questionDiv = document.createElement('div');
-        questionDiv.classList.add('question');
-        questionDiv.innerHTML = `<p>${question.question}</p>`;
+      const scoreDisplay = document.getElementById('score-display');
+        const correctCountSpan = document.getElementById('correct-count');
+        const incorrectCountSpan = document.getElementById('incorrect-count');
+         const emptyCountSpan = document.getElementById('empty-count');
+         const allCountSpan = document.getElementById('all-count');
+     quizContainer.innerHTML = ''; // Clear previous questions
+     currentQuestions.forEach((question, index) => {
+         const questionDiv = document.createElement('div');
+         questionDiv.classList.add('question');
+         questionDiv.innerHTML = `<p>${question.question}</p>`;
 
-        const optionsDiv = document.createElement('div');
-        optionsDiv.classList.add('options');
-        question.options.forEach((option, optionIndex) => {
-            const input = document.createElement('input');
-            input.type = 'radio';
-            input.name = `question-${index}`;
-            input.value = optionIndex;
-            input.id = `question-${index}-option-${optionIndex}`;
+         const optionsDiv = document.createElement('div');
+         optionsDiv.classList.add('options');
 
-            input.addEventListener('change', () => checkAnswer(index, optionIndex));
+         question.options.forEach((option, optionIndex) => {
+             const input = document.createElement('input');
+             input.type = 'radio';
+             input.name = `question-${index}`;
+             input.value = optionIndex;
+             input.id = `question-${index}-option-${optionIndex}`;
 
-            const label = document.createElement('label');
-            label.htmlFor = `question-${index}-option-${optionIndex}`;
-            label.textContent = option;
+             const label = document.createElement('label');
+             label.htmlFor = `question-${index}-option-${optionIndex}`;
+             label.textContent = option;
 
-            const optionDiv = document.createElement('div');
-            optionDiv.appendChild(input);
-            optionDiv.appendChild(label);
-            optionsDiv.appendChild(optionDiv);
-        });
+             const optionDiv = document.createElement('div');
+             optionDiv.appendChild(input);
+             optionDiv.appendChild(label);
+             optionsDiv.appendChild(optionDiv);
 
-        questionDiv.appendChild(optionsDiv);
-        quizContainer.appendChild(questionDiv);
+         });
+         optionsDiv.addEventListener('click', (event) => {
+             const target = event.target.closest('div');
+             if (target) {
+                 const input = target.querySelector('input[type="radio"]');
+                 if(input){
+                     input.checked = true;
+                    checkAnswer(index, parseInt(input.value));
+                 }
+            }
+         });
+         questionDiv.appendChild(optionsDiv);
+         quizContainer.appendChild(questionDiv);
 
-        const explanationDiv = document.createElement('div');
-        explanationDiv.classList.add('explanation');
-        explanationDiv.style.display = 'none';
-        setExplanationText(explanationDiv, question);
-        questionDiv.appendChild(explanationDiv);
-    });
-    updateExplanationsVisibility();
-    updateScoreDisplay();
-}
+         const explanationDiv = document.createElement('div');
+         explanationDiv.classList.add('explanation');
+         explanationDiv.style.display = 'none';
+         setExplanationText(explanationDiv, question);
+         questionDiv.appendChild(explanationDiv);
+     });
+     updateExplanationsVisibility();
+     updateScoreDisplay();
+ }
 
 function checkAnswer(questionIndex, selectedOption) {
     const questionDiv = document.querySelector(`#quiz-container > div:nth-child(${questionIndex + 1})`);
@@ -91,8 +133,8 @@ function checkAnswer(questionIndex, selectedOption) {
     const previousAnswer = userAnswers[questionIndex];
     userAnswers[questionIndex] = parseInt(selectedOption);
 
-    if (parseInt(selectedOption) === questions[questionIndex].answer) {
-        if (previousAnswer !== questions[questionIndex].answer) {
+    if (parseInt(selectedOption) === currentQuestions[questionIndex].answer) {
+        if (previousAnswer !== currentQuestions[questionIndex].answer) {
             correctAnswers++;
         }
 
@@ -103,20 +145,24 @@ function checkAnswer(questionIndex, selectedOption) {
         }
         explanationDiv.innerHTML = "";
     } else {
-        if (previousAnswer === questions[questionIndex].answer) {
+        if (previousAnswer === currentQuestions[questionIndex].answer) {
             correctAnswers--;
         }
         questionDiv.classList.add('incorrect');
         questionDiv.classList.remove('correct');
         explanationDiv.style.display = alwaysShowExplanations ? 'block' : (showExplanations ? 'block' : 'none');
-        setExplanationText(explanationDiv, questions[questionIndex]);
+        setExplanationText(explanationDiv, currentQuestions[questionIndex]);
     }
     updateExplanationsVisibility();
     updateScoreDisplay();
 }
 
 function setExplanationText(explanationDiv, question) {
-    explanationDiv.innerHTML = `<p>Explanation: ${question.explanation || "No explanation provided"}</p>`;
+    const explanationText = question.explanation || "No explanation provided";
+        let formattedExplanation = explanationText.replace(/`{3}java\n([\s\S]*?)`{3}/gs, (match, code) => {
+             return `<pre><code>${code.trim()}</code></pre>`;
+        });
+     explanationDiv.innerHTML = `<p>Explanation: ${formattedExplanation}</p>`;
 }
 
 function updateExplanationsVisibility() {
@@ -125,7 +171,7 @@ function updateExplanationsVisibility() {
         const questionDiv = document.querySelector(`#quiz-container > div:nth-child(${index + 1})`);
         if (alwaysShowExplanations) {
             explanation.style.display = 'block';
-            setExplanationText(explanation, questions[index]);
+            setExplanationText(explanation, currentQuestions[index]);
         } else if (showExplanations && questionDiv.classList.contains('incorrect')) {
             explanation.style.display = 'block';
         } else {
@@ -133,51 +179,82 @@ function updateExplanationsVisibility() {
         }
     });
 }
+ function resetQuiz() {
+    questionsAnswered = 0;
+      correctAnswers = 0;
+      quizContainer.innerHTML = ''; // Clear previous questions
+      userAnswers = new Array(currentQuestions.length).fill(null);
+    const scoreDisplay = document.getElementById('score-display');
+       if(scoreDisplay){
+        scoreDisplay.innerHTML = ``;
+         }
+
+     updateScoreDisplay();
+ }
+
 
 function updateScoreDisplay() {
+    const scoreDisplay = document.getElementById('score-display');
+     const correctCountSpan = document.getElementById('correct-count');
+        const incorrectCountSpan = document.getElementById('incorrect-count');
+         const emptyCountSpan = document.getElementById('empty-count');
+         const allCountSpan = document.getElementById('all-count');
+
     const incorrectAnswers = questionsAnswered - correctAnswers;
-    const emptyAnswers = questions.length - questionsAnswered;
-    correctCountSpan.textContent = correctAnswers;
-    incorrectCountSpan.textContent = incorrectAnswers;
-    emptyCountSpan.textContent = emptyAnswers;
-    allCountSpan.textContent = questions.length;
-    scoreDisplay.innerHTML = ``;
-}
-
-function filterQuestions(filterType) {
-    const allQuestionDivs = document.querySelectorAll('#quiz-container > div');
-
-    allQuestionDivs.forEach((questionDiv, index) => {
-        const selectedOption = document.querySelector(`input[name="question-${index}"]:checked`);
-
-        if (filterType === 'all') {
-            questionDiv.style.display = 'block';
-        } else if (filterType === 'correct') {
-            if (selectedOption && parseInt(selectedOption.value) === questions[index].answer) {
-                questionDiv.style.display = 'block';
-            } else {
-                questionDiv.style.display = 'none';
-            }
-        } else if (filterType === 'incorrect') {
-            if (selectedOption && parseInt(selectedOption.value) !== questions[index].answer) {
-                questionDiv.style.display = 'block';
-            } else {
-                questionDiv.style.display = 'none';
-            }
-        } else if (filterType === 'empty') {
-            if (!selectedOption) {
-                questionDiv.style.display = 'block';
-            } else {
-                questionDiv.style.display = 'none';
-            }
+    const emptyAnswers = currentQuestions.length - questionsAnswered;
+    if (correctCountSpan) {
+          correctCountSpan.textContent = correctAnswers;
         }
-    });
+        if (incorrectCountSpan) {
+          incorrectCountSpan.textContent = incorrectAnswers;
+        }
+       if (emptyCountSpan) {
+        emptyCountSpan.textContent = emptyAnswers;
+      }
+        if (allCountSpan) {
+          allCountSpan.textContent = currentQuestions.length;
+        }
+         if(scoreDisplay){
+            scoreDisplay.innerHTML = ``;
+         }
+
 }
+
+
+ function filterQuestions(filterType) {
+     const allQuestionDivs = document.querySelectorAll('#quiz-container > div');
+
+     allQuestionDivs.forEach((questionDiv, index) => {
+         const selectedOption = document.querySelector(`input[name="question-${index}"]:checked`);
+
+         if (filterType === 'all') {
+             questionDiv.style.display = 'block';
+         } else if (filterType === 'correct') {
+             if (selectedOption && parseInt(selectedOption.value) === currentQuestions[index].answer) {
+                 questionDiv.style.display = 'block';
+             } else {
+                 questionDiv.style.display = 'none';
+             }
+         } else if (filterType === 'incorrect') {
+             if (selectedOption && parseInt(selectedOption.value) !== currentQuestions[index].answer) {
+                 questionDiv.style.display = 'block';
+             } else {
+                 questionDiv.style.display = 'none';
+             }
+         } else if (filterType === 'empty') {
+             if (!selectedOption) {
+                 questionDiv.style.display = 'block';
+             } else {
+                 questionDiv.style.display = 'none';
+             }
+         }
+     });
+ }
 function checkAllCorrectAnswers(simply) {
     const allQuestionDivs = document.querySelectorAll('#quiz-container > div');
     allQuestionDivs.forEach((questionDiv, index) => {
           const optionsDiv = questionDiv.querySelector('.options');
-          const correctInput = optionsDiv.querySelector(`input[name="question-${index}"][value="${questions[index].answer}"]`);
+          const correctInput = optionsDiv.querySelector(`input[name="question-${index}"][value="${currentQuestions[index].answer}"]`);
 
           if(correctInput){
             correctInput.click();
@@ -185,18 +262,16 @@ function checkAllCorrectAnswers(simply) {
           }
 
          if (simply) {
-             optionsDiv.querySelectorAll(`input:not([value="${questions[index].answer}"])`).forEach(input => input.closest('div').style.display = 'none');
+             optionsDiv.querySelectorAll(`input:not([value="${currentQuestions[index].answer}"])`).forEach(input => input.closest('div').style.display = 'none');
         } else {
              optionsDiv.querySelectorAll('input').forEach(input => input.closest('div').style.display = 'block');
        }
      });
 }
 
-
-
 function showResults() {
     let correctAnswers = 0;
-    questions.forEach((question, index) => {
+    currentQuestions.forEach((question, index) => {
         const selectedOption = document.querySelector(`input[name="question-${index}"]:checked`);
 
         if (selectedOption) {
@@ -207,9 +282,9 @@ function showResults() {
         }
     });
 
-    const incorrectAnswers = questions.length - correctAnswers;
+    const incorrectAnswers = currentQuestions.length - correctAnswers;
     resultDiv.innerHTML = `You got <span class='correct-text'>${correctAnswers} correct</span> and <span class='incorrect-text'>${incorrectAnswers} incorrect</span>`;
 }
 
-buildQuiz();
+ buildQuiz(); // Build the initial quiz
 submitButton.addEventListener('click', showResults);
